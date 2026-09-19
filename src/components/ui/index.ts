1cc05dac78a61c59df0fcb6be72c4e7d447c0428
Ritz Carl Feliciano <rc.feliciano013097@gmail.com>
2026-09-19 23:23:25 +0800

    feat: add reusable index.ts

---
+++ src/components/ui/index.ts
@@ -0,0 +1,5 @@
+export { Badge } from "./badge";
+export { Button } from "./button";
+export type { ButtonProps } from "./button";
+export { Card } from "./card";
+export { Section } from "./section";
